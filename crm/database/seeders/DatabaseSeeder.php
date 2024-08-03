08ecac0855fe67b6a617a9cc787efcee9c2e94ed
Paul Co <79306634+CohPhol@users.noreply.github.com>
2024-08-03 20:20:14 +0800

    Experiment

--- crm/database/seeders/DatabaseSeeder.php
+++ crm/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +14,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => bcrypt('admin@admin.com'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        echo "User has been seeded. \n";
     }
 }
